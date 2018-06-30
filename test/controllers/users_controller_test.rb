097fd8041565740d25f49be71ba8e5696a9a75f9
Yoshihisa Murakami <muranet+git@gmail.com>
2018-06-30 20:13:38 +0900

    新規ユーザー作成画面（仮）作成

---
+++ test/controllers/users_controller_test.rb
@@ -0,0 +1,10 @@
+require 'test_helper'
+
+class UsersControllerTest < ActionDispatch::IntegrationTest
+  test "新規ユーザー作成画面にアクセスできること" do
+    get signup_path
+    assert_response :success
+    assert_select 'title', '新規ユーザー作成 - 家計簿App'
+  end
+
+end
