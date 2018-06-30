e8879342146bcbe48547adc8d8f22c5637322af6
Yoshihisa Murakami <muranet+git@gmail.com>
2018-06-30 19:56:02 +0900

    [static_pages] タイトルをページごとに変えるよう変更

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def full_title(page_title='')
+    base_title = '家計簿App'
+    if page_title.empty?
+      base_title
+    else
+      page_title + " - " + base_title
+    end
+  end
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,11 +4,13 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "/ にアクセスできること" do
     get root_path
     assert_response :success
+    assert_select "title", "家計簿の入力 - 家計簿App"
   end
 
   test "/help ページにアクセスできること" do
     get help_path
     assert_response :success
+    assert_select "title", "ヘルプ - 家計簿App"
   end
 
 end
