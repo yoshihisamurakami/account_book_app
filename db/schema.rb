d74324e42f16988282fda3157d6034cd554b5b5b
Yoshihisa Murakami <muranet+git@gmail.com>
2018-09-05 05:59:19 +0000

    userテーブルにadminカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180630124704) do
+ActiveRecord::Schema.define(version: 20180905055605) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20180630124704) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.boolean "admin", default: false
   end
 
 end
