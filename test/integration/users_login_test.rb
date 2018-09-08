cedc7c78f927b15bac1d7a31cdcc190b009351ab
Yoshihisa Murakami <muranet+git@gmail.com>
2018-09-08 09:44:38 +0000

    ヘッダーメニューに管理者用メニューを追加

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -2,6 +2,7 @@ require 'test_helper'
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
   def setup
+    @admin_user = users(:admin)
     @user = users(:murakami)
   end
   
@@ -25,5 +26,16 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", login_path, count: 0
     assert_select "a[href=?]", logout_path
     #assert_select "a[href=?]", user_path(@user)
+  end
+  
+  test "管理者ユーザーでログインしたときのレイアウト" do
+    log_in_as(@admin_user)
+    get root_url
+    assert_select "a[href=?]", login_path, count: 0
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", '#', text: "管理者用"
+    assert_select "a[href=?]", users_path, text: "ユーザー一覧"
+    #assert_select "a[href=?]", '#', text: "口座一覧"
+    #assert_select "a[href=?]", '#', text: "カテゴリ一覧"
   end
 end
