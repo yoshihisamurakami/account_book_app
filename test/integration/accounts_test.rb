41558bf4674ad4f9186919f130cb2b610c10b6e6
Yoshihisa Murakami <muranet+git@gmail.com>
2018-09-29 09:29:33 +0000

    口座情報の編集

--- test/integration/accounts_test.rb
+++ test/integration/accounts_test.rb
@@ -4,6 +4,7 @@ class AccountsTest < ActionDispatch::IntegrationTest
   def setup
     @admin_user = users(:admin)
     @user = users(:murakami)
+    @account = accounts(:yoshihisa_bank)
   end
   
   test "口座一覧ページが正常であること" do
@@ -43,5 +44,36 @@ class AccountsTest < ActionDispatch::IntegrationTest
     follow_redirect!
     assert_template 'accounts/index'
     assert_not flash.empty?
+  end
+  
+  test "口座編集失敗" do
+    log_in_as(@admin_user)
+    get edit_account_path(@account)
+    assert_template 'accounts/edit'
+
+    patch account_path(@account), params: { account: { name:  "",
+                                              comment: "",
+                                          } }
+
+    assert_template 'accounts/edit'
+    assert_select 'div#error_explanation', text: /エラーがみつかりました/
+    assert_select 'div.alert'
+    assert_select 'div.alert-danger'
+  end
+  
+  test "口座編集成功" do
+    log_in_as(@admin_user)
+    get edit_account_path(@account)
+    assert_template 'accounts/edit'
+    name  = "村上銀行"
+    comment = "コメント"
+    patch account_path(@account), params: { account: { name:  name,
+                                              comment: comment,
+                                          } }
+    assert_not flash.empty?
+    assert_redirected_to accounts_path
+    @account.reload
+    assert_equal name,  @account.name
+    assert_equal comment, @account.comment
   end
 end
