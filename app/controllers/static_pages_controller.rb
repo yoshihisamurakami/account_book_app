23b0ac3abc37faf7361d23ad79bb705b7fae5ec2
Yoshihisa Murakami <muranet+git@gmail.com>
2018-11-03 13:19:26 +0000

    家計簿入力画面の作成

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,6 +2,8 @@ class StaticPagesController < ApplicationController
   before_action :require_logged_in, only: [:home]
   
   def home
+    @book = current_user.books.build
+    @book.books_date = '2018/11/03'
   end
 
   def help
