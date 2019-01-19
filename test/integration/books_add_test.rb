0334045b9cbb191685c6207269646c36081e0af9
Yoshihisa Murakami <muranet+git@gmail.com>
2019-01-19 21:53:04 +0900

    booksデータ追加のテストを追加

--- test/integration/books_add_test.rb
+++ test/integration/books_add_test.rb
@@ -7,6 +7,7 @@ class BooksTest < ActionDispatch::IntegrationTest
     @user = users(:murakami)
     @account = accounts(:yoshihisa_bank)
     @category = categories(:daily_goods)
+    @book_pay = books(:buy_daily_goods)
   end
 
   test "未ログイン状態で/ページを開いたらリダイレクトされること" do
@@ -14,7 +15,7 @@ class BooksTest < ActionDispatch::IntegrationTest
     assert_redirected_to login_url
   end
 
-  test "カテゴリがなしでも入金してOK" do
+  test "10000円入金したあと、1000円出金する" do
     log_in_as(@user)
     assert_difference 'Book.count', 1 do
     post books_path, params: { book: {
@@ -31,5 +32,42 @@ class BooksTest < ActionDispatch::IntegrationTest
       special: false,
     } }
     end
+    assert_equal @account.balance, 10000
+    assert_difference 'Book.count', 1 do
+    post books_path, params: { book: {
+      books_date:  "2018-11-26",
+      user_id: @user.id,
+      account_id: @account.id,
+      deposit: false,
+      transfer: false,
+      category_id: @category.id,
+      summary: '出金',
+      amount: 1000,
+      common: false,
+      business: false,
+      special: false,
+    } }
+    end
+    assert_equal @account.balance, 9000
   end
+
+  test "update成功" do
+    log_in_as(@user)
+    get edit_book_path(@book_pay.id)
+    assert_template 'static_pages/home'
+    # post edit_book_path(@book_pay.id), params: { book: {
+    #   books_date: "2018-10-28",
+    #   user: users(:murakami),
+    #   account: accounts(:yoshihisa_wallet),
+    #   deposit: false,
+    #   transfer: false,
+    #   category: categories(:daily_goods),
+    #   summary: 'ウェットティッシュ',
+    #   amount: 1000,
+    #   common: true,
+    #   business: false,
+    #   special: false ,
+    #   }}
+  end
+
 end
