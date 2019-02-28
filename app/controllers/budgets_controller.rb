d2d3cf94df849d988f8d4dbaf6594dfee302eef0
Yoshihisa Murakami <muranet+git@gmail.com>
2019-02-28 07:02:08 +0000

    予算新規作成

--- app/controllers/budgets_controller.rb
+++ app/controllers/budgets_controller.rb
@@ -2,6 +2,17 @@ class BudgetsController < ApplicationController
   before_action :require_logged_in
 
   def new
+    @budget = Budget.new
+  end
+
+  def create
+    @budget = Budget.new(budget_params)
+    if @budget.save
+      flash[:success] = "保存されました。"
+      redirect_to budgets_path
+    else
+      render 'new'
+    end
   end
 
   def index
@@ -9,4 +20,19 @@ class BudgetsController < ApplicationController
       .order(:target_year, :target_month)
   end
 
+  private
+
+  def budget_params
+    params.require(:budget).permit(
+      :target_year,
+      :target_month,
+      :regular_income,
+      :extra_income,
+      :special_income,
+      :tax_funding,
+      :special_funding,
+      :savings,
+      :fixed_budget
+    )
+  end
 end
