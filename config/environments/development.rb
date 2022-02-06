27cab8ed45888ff1fa5847acb8812954b59928a5
muranet+git@gmail.com <muranet+git@gmail.com>
2022-02-06 15:20:32 +0900

    development環境の整備、csvダウンロードエラーの解消 test環境の整備

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,4 +59,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  config.hosts << '127.0.0.1:3000'
+  config.hosts << 'localhost:3000'
 end
