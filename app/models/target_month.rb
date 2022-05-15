42b5e04a60b42b19657d29a2c51a98a39a27b20e
muranet+git@gmail.com <muranet+git@gmail.com>
2022-05-15 18:06:01 +0900

    :art: target_term_model を target_month へ置き換え

--- app/models/target_month.rb
+++ app/models/target_month.rb
@@ -13,8 +13,26 @@ class TargetMonth
     session[:target_term_month] || Date.today.month
   end
 
+  def move_to_prev_month!
+    prev_month_first_day = first_day.prev_month
+    session[:target_term_year]  = prev_month_first_day.year
+    session[:target_term_month] = prev_month_first_day.month
+  end
+
+  def move_to_next_month!
+    next_month_first_day = first_day.next_month
+    session[:target_term_year]  = next_month_first_day.year
+    session[:target_term_month] = next_month_first_day.month
+  end
+
   def lastday_of_month
     Date.new(year, month, -1).day
+  end
+
+  private
+
+  def first_day
+    Date.new(year, month, 1)
   end
 
 end
