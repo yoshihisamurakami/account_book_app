113b7833b9af602b72e265ba5ac400a72f63a952
muranet+git@gmail.com <muranet+git@gmail.com>
2023-02-11 22:33:50 +0900

    開発環境でdocker postgresを使うよう変更

--- db/schema.rb
+++ db/schema.rb
@@ -2,24 +2,23 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# This file is the source Rails uses to define your schema when running `rails
-# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
 # be faster and is potentially less error prone than running all of your
 # migrations from scratch. Old migrations may fail to apply correctly if those
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_04_105526) do
-
+ActiveRecord::Schema[7.0].define(version: 2021_04_04_105526) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "accounts", force: :cascade do |t|
     t.string "name"
     t.string "comment"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
   end
 
   create_table "books", force: :cascade do |t|
@@ -34,8 +33,8 @@ ActiveRecord::Schema.define(version: 2021_04_04_105526) do
     t.boolean "common"
     t.boolean "business"
     t.boolean "special"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.index ["account_id"], name: "index_books_on_account_id"
     t.index ["books_date", "updated_at"], name: "index_books_on_books_date_and_updated_at"
     t.index ["category_id"], name: "index_books_on_category_id"
@@ -52,8 +51,8 @@ ActiveRecord::Schema.define(version: 2021_04_04_105526) do
     t.integer "special_funding"
     t.integer "savings"
     t.integer "fixed_budget"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
   end
 
   create_table "categories", force: :cascade do |t|
@@ -61,15 +60,15 @@ ActiveRecord::Schema.define(version: 2021_04_04_105526) do
     t.boolean "is_tax"
     t.boolean "is_fixed"
     t.boolean "is_food"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
   end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.string "password_digest"
     t.boolean "admin", default: false
   end
